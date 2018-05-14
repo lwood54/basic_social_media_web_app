52eb13c749e8e024221a1991396215c5e3882395
Logan Wood <lwood3499@gmail.com>
2018-05-13 20:33:06 -0500

    added post route for user registration with password hashing

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 // define variables that will direct routes to proper directory
 const posts = require('./routes/api/posts');
@@ -8,6 +9,12 @@ const users = require('./routes/api/users');
 
 
 const app = express();
+
+// Body Parser middleware
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(bodyParser.json());
 
 // DB Config
 const db = require('./config/keys').mongoURI;
